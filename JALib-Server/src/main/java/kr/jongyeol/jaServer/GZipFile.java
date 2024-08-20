fc0cf4bd76ab57f8a095172f8fffef05e3308d17
Jongyeol <bcpjy1233@gmail.com>
2024-08-21 08:51:40 +0900

    feat: gzip level to 9

--- JALib-Server/src/main/java/kr/jongyeol/jaServer/GZipFile.java
+++ JALib-Server/src/main/java/kr/jongyeol/jaServer/GZipFile.java
@@ -6,12 +6,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.Deflater;
 import java.util.zip.GZIPOutputStream;
 
 public class GZipFile {
     public static void gzipFile(File source, File destination) throws IOException {
         byte[] buffer = new byte[1024];
-        @Cleanup GZIPOutputStream gzipOuputStream = new GZIPOutputStream(new FileOutputStream(destination));
+        @Cleanup GZIPOutputStream gzipOuputStream = new GZIPOutputStream(new FileOutputStream(destination)) {{
+            def.setLevel(Deflater.BEST_COMPRESSION);
+        }};
         @Cleanup FileInputStream fileInput = new FileInputStream(source);
         int bytes_read;
         while((bytes_read = fileInput.read(buffer)) > 0) gzipOuputStream.write(buffer, 0, bytes_read);
