557ea32f3ae1ea61184400a7c47c1da84978485f
Jongyeol <bcpjy1233@gmail.com>
2024-09-18 20:49:25 +0900

    refactor: Change bootstrap project name
    JALib-Bootstrap -> JaLib.Bootstrap

--- JALib.Bootstrap/Properties/AssemblyInfo.cs
+++ JALib.Bootstrap/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JALib_Bootstrap")]
+[assembly: AssemblyTitle("JALib.Bootstrap")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JALib_Bootstrap")]
+[assembly: AssemblyProduct("JALib.Bootstrap")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
