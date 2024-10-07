6f773fb3f3acfe4bbc86aaec2e39f63983eb1c36
Jongyeol <bcpjy1233@gmail.com>
2024-10-07 09:06:11 +0900

    chore: Bootstrap Integration

---
+++ Bootstrap/Bootstrap.cs
@@ -0,0 +1,8 @@
+﻿using JALib.Bootstrap;
+using UnityModManagerNet;
+
+namespace Bootstrap {
+    public class Bootstrap {
+        public static void Setup(UnityModManager.ModEntry modEntry) => JABootstrap.Load(modEntry);
+    }
+}
