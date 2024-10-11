c60178b9f33275f5352d837027599ad5ee239dbe
Jongyeol <bcpjy1233@gmail.com>
2024-10-11 19:42:06 +0900

    refactor: Auto Refactor Rider

--- JALib/JALib.cs
+++ JALib/JALib.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using HarmonyLib;
 using JALib.API;
