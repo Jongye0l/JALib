c0280872acedc9d689d9fce1a36dd0e297e9fbb5
Jongyeol <bcpjy1233@gmail.com>
2024-10-16 13:06:05 +0900

    feat: Prefix Now Try Catch In Patch Method

--- JALib/Core/Patch/JAEmitter.cs
+++ JALib/Core/Patch/JAEmitter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using JALib.Tools;
@@ -12,8 +13,10 @@ class JAEmitter(object original) {
     public void Emit(OpCode opcode, long arg) => original.Invoke("Emit", [typeof(OpCode), typeof(long)], opcode, arg);
     public void Emit(OpCode opcode, float arg) => original.Invoke("Emit", [typeof(OpCode), typeof(float)], opcode, arg);
     public void Emit(OpCode opcode, double arg) => original.Invoke("Emit", [typeof(OpCode), typeof(double)], opcode, arg);
+    public void Emit(OpCode opcode, string arg) => original.Invoke("Emit", [typeof(OpCode), typeof(string)], opcode, arg);
     public void Emit(OpCode opcode, Label arg) => original.Invoke("Emit", [typeof(OpCode), typeof(Label)], opcode, arg);
     public void Emit(OpCode opcode, Type arg) => original.Invoke("Emit", [typeof(OpCode), typeof(Type)], opcode, arg);
+    public void Emit(OpCode opcode, MethodInfo arg) => original.Invoke("Emit", [typeof(OpCode), typeof(MethodInfo)], opcode, arg);
     public void Emit(OpCode opcode, LocalBuilder arg) => original.Invoke("Emit", [typeof(OpCode), typeof(LocalBuilder)], opcode, arg);
     public void MarkBlockBefore(ExceptionBlock block) => original.Invoke("MarkBlockBefore", [typeof(ExceptionBlock), typeof(Label).MakeByRefType()], block, null);
     public void MarkLabel(Label label) => original.Invoke("MarkLabel", [typeof(Label)], label);
--- JALib/Core/Patch/JAMethodPatcher.cs
+++ JALib/Core/Patch/JAMethodPatcher.cs
@@ -9,39 +9,41 @@ using MonoMod.Utils;
 namespace JALib.Core.Patch;
 
 class JAMethodPatcher {
-    private readonly bool debug;
-    private readonly JAEmitter emitter;
-    private readonly List<MethodInfo> finalizers;
-    private readonly int idx;
-    private readonly ILGenerator il;
     private readonly MethodBase original;
-    private readonly object originalPatcher;
-    private readonly List<MethodInfo> postfixes;
-    private List<MethodInfo> prefixes;
-    private readonly List<MethodInfo> removes;
-    private readonly List<MethodInfo> replaces;
-    private readonly Type returnType;
     private readonly MethodBase source;
-    private readonly List<MethodInfo> transpilers;
+    private readonly bool debug;
+    private HarmonyLib.Patch[] prefixes;
+    private HarmonyLib.Patch[] postfixes;
+    private HarmonyLib.Patch[] transpilers;
+    private HarmonyLib.Patch[] finalizers;
+    private HarmonyLib.Patch[] removes;
+    private HarmonyLib.Patch[] replaces;
+    private readonly object originalPatcher;
+    private readonly ILGenerator il;
+    private readonly int idx;
+    private readonly JAEmitter emitter;
+    private readonly Type returnType;
     private readonly bool useStructReturnBuffer;
+    private readonly JAPatchInfo jaPatchInfo;
 
     public JAMethodPatcher(MethodBase original, MethodBase source, PatchInfo patchInfo, JAPatchInfo jaPatchInfo) {
         this.original = original;
         this.source = source;
         debug = patchInfo.Debugging || Harmony.DEBUG;
-        prefixes = SortPatchMethods(original, patchInfo.prefixes, debug);
-        postfixes = SortPatchMethods(original, patchInfo.postfixes, debug);
-        transpilers = SortPatchMethods(original, patchInfo.transpilers, debug);
-        finalizers = SortPatchMethods(original, patchInfo.finalizers, debug);
-        replaces = SortPatchMethods(original, jaPatchInfo.replaces, debug);
-        removes = SortPatchMethods(original, jaPatchInfo.removes, debug);
+        List<MethodInfo> prefix = SortPatchMethods(original, patchInfo.prefixes.Concat(jaPatchInfo.tryPrefixes).Concat(jaPatchInfo.removes).ToArray(), debug, out prefixes);
+        List<MethodInfo> postfix = SortPatchMethods(original, patchInfo.postfixes.Concat(jaPatchInfo.tryPostfixes).ToArray(), debug, out postfixes);
+        List<MethodInfo> transpiler = SortPatchMethods(original, patchInfo.transpilers, debug, out transpilers);
+        List<MethodInfo> finalizer = SortPatchMethods(original, patchInfo.finalizers, debug, out finalizers);
+        SortPatchMethods(original, jaPatchInfo.replaces, debug, out replaces);
+        SortPatchMethods(original, jaPatchInfo.removes, debug, out removes);
         SetupPrefixRemove();
-        originalPatcher = typeof(Harmony).Assembly.GetType("HarmonyLib.MethodPatcher").New(original, source, prefixes, postfixes, transpilers, finalizers, debug);
+        originalPatcher = typeof(Harmony).Assembly.GetType("HarmonyLib.MethodPatcher").New(original, source, prefix, postfix, transpiler, finalizer, debug);
         il = originalPatcher.GetValue<ILGenerator>("il");
-        idx = prefixes.Count + postfixes.Count + finalizers.Count;
+        idx = prefixes.Length + postfixes.Length + finalizers.Length;
         emitter = new JAEmitter(originalPatcher.GetValue("emitter"));
         useStructReturnBuffer = originalPatcher.GetValue<bool>("useStructReturnBuffer");
         returnType = original is MethodInfo info ? info.ReturnType : typeof(void);
+        this.jaPatchInfo = jaPatchInfo;
     }
 
     private void SetupPrefixRemove() {
@@ -50,23 +52,27 @@ class JAMethodPatcher {
             if(a) return false;
             if(removes.Contains(pre)) a = true;
             return true;
-        }).ToList();
+        }).ToArray();
     }
 
-    private static List<MethodInfo> SortPatchMethods(MethodBase original, HarmonyLib.Patch[] patches, bool debug) =>
-        typeof(Harmony).Assembly.GetType("HarmonyLib.PatchSorter").New(patches, debug).Invoke<List<MethodInfo>>("Sort", original);
+    private static List<MethodInfo> SortPatchMethods(MethodBase original, HarmonyLib.Patch[] patches, bool debug, out HarmonyLib.Patch[] sortedPatches) {
+        object patchSorter = typeof(Harmony).Assembly.GetType("HarmonyLib.PatchSorter").New(patches, debug);
+        List<MethodInfo> sortedMethods = patchSorter.Invoke<List<MethodInfo>>("Sort", original);
+        sortedPatches = patchSorter.GetValue<HarmonyLib.Patch[]>("sortedPatchArray");
+        return sortedMethods;
+    }
 
     internal MethodInfo CreateReplacement(out Dictionary<int, CodeInstruction> finalInstructions) {
         Type methodPatcher = typeof(Harmony).Assembly.GetType("HarmonyLib.MethodPatcher");
-        LocalBuilder[] originalVariables = removes.Count > 0 ? [] : methodPatcher.Invoke<LocalBuilder[]>("DeclareLocalVariables", il, replaces.Count > 0 ? replaces.Last() : source ?? original);
+        LocalBuilder[] originalVariables = removes.Length > 0 ? [] : methodPatcher.Invoke<LocalBuilder[]>("DeclareLocalVariables", il, replaces.Length > 0 ? replaces.Last() : source ?? original);
         Dictionary<string, LocalBuilder> privateVars = new();
-        List<MethodInfo> fixes = removes.Count == 0 ? prefixes.Union(postfixes).Union(finalizers).ToList() : prefixes;
+        HarmonyLib.Patch[] fixes = removes.Length == 0 ? prefixes.Union(postfixes).Union(finalizers).ToArray() : prefixes;
         LocalBuilder resultVariable = null;
         if(idx > 0) {
             resultVariable = DeclareLocalVariable(returnType, true);
             privateVars["__result"] = resultVariable;
         }
-        if(fixes.Any(fix => fix.GetParameters().Any(p => p.Name == "__args"))) {
+        if(fixes.Any(fix => fix.PatchMethod.GetParameters().Any(p => p.Name == "__args"))) {
             originalPatcher.Invoke("PrepareArgumentArray");
             LocalBuilder local2 = il.DeclareLocal(typeof(object[]));
             emitter.Emit(OpCodes.Stloc, local2);
@@ -75,34 +81,35 @@ class JAMethodPatcher {
         Label? skipOriginalLabel = null;
         LocalBuilder runOriginalVariable = null;
         bool prefixAffectsOriginal = prefixes.Any(fix => methodPatcher.Invoke<bool>("PrefixAffectsOriginal", [fix]));
-        bool anyFixHasRunOriginalVar = fixes.Any(fix => fix.GetParameters().Any(p => p.Name == "__runOriginal"));
+        bool anyFixHasRunOriginalVar = fixes.Any(fix => fix.PatchMethod.GetParameters().Any(p => p.Name == "__runOriginal"));
         if(prefixAffectsOriginal || anyFixHasRunOriginalVar) {
             runOriginalVariable = DeclareLocalVariable(typeof(bool));
             emitter.Emit(OpCodes.Ldc_I4_1);
             emitter.Emit(OpCodes.Stloc, runOriginalVariable);
             if(prefixAffectsOriginal) skipOriginalLabel = il.DefineLabel();
         }
-        fixes.ForEach(fix => {
-            if(fix.DeclaringType == null || privateVars.ContainsKey(fix.DeclaringType.AssemblyQualifiedName)) return;
-            fix.GetParameters().Where(patchParam => patchParam.Name == "__state")
-                .Do(patchParam => privateVars[fix.DeclaringType.AssemblyQualifiedName] =
+        foreach(HarmonyLib.Patch fix in fixes) {
+            MethodInfo method = fix.PatchMethod;
+            if(method.DeclaringType == null || privateVars.ContainsKey(method.DeclaringType.AssemblyQualifiedName)) continue;
+            method.GetParameters().Where(patchParam => patchParam.Name == "__state")
+                .Do(patchParam => privateVars[method.DeclaringType.AssemblyQualifiedName] =
                                       DeclareLocalVariable(patchParam.ParameterType));
-        });
+        }
         LocalBuilder finalizedVariable = null;
-        if(finalizers.Count > 0) {
+        if(finalizers.Length > 0) {
             finalizedVariable = DeclareLocalVariable(typeof(bool));
             privateVars["__exception"] = DeclareLocalVariable(typeof(Exception));
             emitter.MarkBlockBefore(new ExceptionBlock(ExceptionBlockType.BeginExceptionBlock));
         }
-        originalPatcher.Invoke("AddPrefixes", privateVars, runOriginalVariable);
+        AddPrefixes(privateVars, runOriginalVariable, methodPatcher);
         if(skipOriginalLabel.HasValue) {
             emitter.Emit(OpCodes.Ldloc, runOriginalVariable);
             emitter.Emit(OpCodes.Brfalse, skipOriginalLabel.Value);
         }
         bool needsToStorePassthroughResult = false;
         bool hasReturnCode = false;
-        if(removes.Count == 0) {
-            JAMethodCopier copier = new(replaces.Count > 0 ? replaces.Last() : source ?? original, il, originalVariables);
+        if(removes.Length == 0) {
+            JAMethodCopier copier = new(replaces.Length > 0 ? replaces.Last().PatchMethod : source ?? original, il, originalVariables);
             copier.SetArgumentShift(useStructReturnBuffer);
             copier.SetDebugging(debug);
             copier.AddTranspiler(transpilers);
@@ -116,7 +123,7 @@ class JAMethodPatcher {
                 emitter.Emit(OpCodes.Ldloc, resultVariable);
             needsToStorePassthroughResult = originalPatcher.Invoke<bool>("AddPostfixes", privateVars, runOriginalVariable, true);
         }
-        bool hasFinalizers = finalizers.Any();
+        bool hasFinalizers = finalizers.Length > 0;
         if(hasFinalizers) {
             if(needsToStorePassthroughResult) {
                 emitter.Emit(OpCodes.Stloc, resultVariable);
@@ -194,5 +201,58 @@ class JAMethodPatcher {
         else emitter.Emit(OpCodes.Ldc_I4, 0);
         emitter.Emit(OpCodes.Stloc, v);
         return v;
+    }
+
+    private void AddPrefixes(Dictionary<string, LocalBuilder> variables, LocalBuilder runOriginalVariable, Type methodPatcher) {
+        foreach(HarmonyLib.Patch patch in prefixes) {
+            MethodInfo fix = patch.PatchMethod;
+            Label? skipLabel = methodPatcher.Invoke<bool>("PrefixAffectsOriginal", [fix]) ? il.DefineLabel() : new Label?();
+            if(skipLabel.HasValue) {
+                emitter.Emit(OpCodes.Ldloc, runOriginalVariable);
+                emitter.Emit(OpCodes.Brfalse, skipLabel.Value);
+            }
+            LocalBuilder exceptionVar = jaPatchInfo.tryPrefixes.Contains(patch) ? il.DeclareLocal(typeof(Exception)) : null;
+            if(exceptionVar != null) emitter.MarkBlockBefore(new ExceptionBlock(ExceptionBlockType.BeginExceptionBlock));
+            List<KeyValuePair<LocalBuilder, Type>> keyValuePairList = [];
+            object[] args = [fix, variables, runOriginalVariable, false, null, keyValuePairList];
+            originalPatcher.Invoke("EmitCallParameter", args);
+            LocalBuilder tmpObjectVar = (LocalBuilder) args[4];
+            emitter.Emit(OpCodes.Call, fix);
+            if(fix.GetParameters().Any(p => p.Name == "__args"))
+                originalPatcher.Invoke("RestoreArgumentArray", variables);
+            if(tmpObjectVar != null) {
+                emitter.Emit(OpCodes.Ldloc, tmpObjectVar);
+                emitter.Emit(OpCodes.Unbox_Any, AccessTools.GetReturnedType(original));
+                emitter.Emit(OpCodes.Stloc, variables["__result"]);
+            }
+            keyValuePairList.Do(tmpBoxVar => {
+                emitter.Emit(this.original.IsStatic ? OpCodes.Ldarg_0 : OpCodes.Ldarg_1);
+                emitter.Emit(OpCodes.Ldloc, tmpBoxVar.Key);
+                emitter.Emit(OpCodes.Unbox_Any, tmpBoxVar.Value);
+                emitter.Emit(OpCodes.Stobj, tmpBoxVar.Value);
+            });
+            Type returnType = fix.ReturnType;
+            if(returnType != typeof(void)) {
+                if(returnType != typeof(bool)) throw new Exception($"Prefix patch {fix} has not \"bool\" or \"void\" return type: {returnType}");
+                emitter.Emit(OpCodes.Stloc, runOriginalVariable);
+            }
+            if(exceptionVar != null) {
+                emitter.MarkBlockBefore(new ExceptionBlock(ExceptionBlockType.BeginCatchBlock));
+                emitter.Emit(OpCodes.Stloc, exceptionVar);
+                emitter.Emit(OpCodes.Ldstr, ((TriedPatchData) patch).mod.Name);
+                emitter.Emit(OpCodes.Call, typeof(JAMod).Method("GetMods", typeof(string)));
+                emitter.Emit(OpCodes.Ldstr, "An error occurred while invoking a prefix patch " + patch.owner);
+                emitter.Emit(OpCodes.Ldloc, exceptionVar);
+                emitter.Emit(OpCodes.Call, typeof(JAMod).Method("LogException", typeof(string), typeof(Exception)));
+                if(returnType == typeof(bool)) {
+                    emitter.Emit(OpCodes.Ldc_I4_1);
+                    emitter.Emit(OpCodes.Stloc, runOriginalVariable);
+                }
+                emitter.MarkBlockAfter(new ExceptionBlock(ExceptionBlockType.EndExceptionBlock));
+            }
+            if(!skipLabel.HasValue) continue;
+            emitter.MarkLabel(skipLabel.Value);
+            emitter.Emit(OpCodes.Nop);
+        }
     }
 }
