9e0344944fb5544a5269ab5818d5b05edf62f2c2
Jongyeol <bcpjy1233@gmail.com>
2024-10-17 09:03:09 +0900

    fix: Unpatch is not work

--- JALib/Core/Patch/ReversePatchData.cs
+++ JALib/Core/Patch/ReversePatchData.cs
@@ -6,5 +6,6 @@ namespace JALib.Core.Patch;
 public class ReversePatchData {
     public MethodBase original;
     public HarmonyMethod patchMethod;
+    public JAReversePatchAttribute attribute;
     public JAMod mod;
 }
