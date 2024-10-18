f3f7f223e8384da1d73f9c3b5fd62b0430015b6b
Jongyeol <bcpjy1233@gmail.com>
2024-10-18 13:34:52 +0900

    feat: Support English Web

---
+++ JALib-Server/src/main/java/kr/jongyeol/jaServer/LocaleConfig.java
@@ -0,0 +1,19 @@
+package kr.jongyeol.jaServer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
+    }
+}
--- JALib-Server/src/main/java/kr/jongyeol/jaServer/controller/ModApplicatorController.java
+++ JALib-Server/src/main/java/kr/jongyeol/jaServer/controller/ModApplicatorController.java
@@ -9,31 +9,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Locale;
+
 @Controller
 public class ModApplicatorController extends CustomController {
     @GetMapping("/modApplicator/{name}/{version}")
-    public String modApplicator(HttpServletRequest request, @PathVariable String name, @PathVariable String version, Model model) {
+    public String modApplicator(HttpServletRequest request, @PathVariable String name, @PathVariable String version, Model model, Locale locale) {
         info(request, "ModApplicator: " + name + " " + version);
         ModData modData = ModData.getModData(name);
-        boolean forceUpdate = true;
+        boolean forceUpdate = false;
+        boolean isKorean = locale == Locale.KOREAN || locale == Locale.KOREA;
         Version ver = new Version(version);
         if(!ver.equals(modData.getVersion()) && !ver.equals(modData.getBetaVersion())) {
             boolean isBeta = modData.getBetaMap().get(ver);
-            if(modData.isForceUpdate()) model.addAttribute("updateType", "모드");
-            else if(modData.isForceUpdateBeta() && isBeta) model.addAttribute("updateType", "베타");
+            forceUpdate = true;
+            if(modData.isForceUpdate()) model.addAttribute("updateType", isKorean ? " 모드" : " mod");
+            else if(modData.isForceUpdateBeta() && isBeta) model.addAttribute("updateType", isKorean ? " 베타" : " beta");
             else {
                 forceUpdate = false;
                 for(ForceUpdateHandle handle : modData.getForceUpdateHandles())
                     if(handle.checkVersion(ver)) forceUpdate = handle.forceUpdate;
-                if(forceUpdate) model.addAttribute("updateType", "버전");
+                if(forceUpdate) model.addAttribute("updateType", isKorean ? " 버전" : " version");
             }
-            if(forceUpdate) model.addAttribute("oldFullName", " " + name + " " + version);
+            if(forceUpdate) model.addAttribute("oldFullName", " " + name + " " + version + (isKorean ? "" : " "));
             version = (isBeta ? modData.getBetaVersion() : modData.getVersion()).toString();
         }
         model.addAttribute("name", name);
         model.addAttribute("version", version);
         model.addAttribute("fullName", " " + name + " " + version);
         model.addAttribute("redirect", forceUpdate);
-        return "ModApplicator-kr";
+        return "ModApplicator-" + (isKorean ? "ko" : "en");
     }
 }
