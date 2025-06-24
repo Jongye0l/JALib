05ec957b104f086cc84520a997ca8a1f636dd857
Jongyeol <bcpjy1233@gmail.com>
2025-06-24 14:26:12 +0900

    feat: zipper support name, data changer

--- JALib/Tools/Zipper.cs
+++ JALib/Tools/Zipper.cs
@@ -15,7 +15,7 @@ public static class Zipper {
 
     public static RawFile[] Unzip(Stream stream) {
         using ZipArchive archive = new(stream, ZipArchiveMode.Read, false, Encoding.UTF8);
-        List<RawFile> files = new();
+        List<RawFile> files = [];
         Dictionary<string, RawFile> folders = new();
         foreach(ZipArchiveEntry entry in archive.Entries) {
             byte[] buffer = new byte[entry.Length];
@@ -44,9 +44,50 @@ public static class Zipper {
         return files.ToArray();
     }
 
+    public static RawFile[] Unzip(byte[] zipData, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        using MemoryStream zipStream = new(zipData);
+        return Unzip(zipStream, nameChanger, dataChanger);
+    }
+
+    public static RawFile[] Unzip(Stream stream, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        using ZipArchive archive = new(stream, ZipArchiveMode.Read, false, Encoding.UTF8);
+        List<RawFile> files = [];
+        Dictionary<string, RawFile> folders = new();
+        foreach(ZipArchiveEntry entry in archive.Entries) {
+            byte[] buffer = new byte[entry.Length];
+            using(Stream st = entry.Open()) st.Read(buffer, 0, buffer.Length);
+            buffer = dataChanger(buffer);
+            string[] path = nameChanger(entry.FullName).Split('/');
+            if(path.Length > 1) {
+                string folder = string.Join("/", path[..^1]);
+                if(!folders.ContainsKey(folder)) {
+                    RawFile parent = null;
+                    for(int i = 0; i < path.Length - 1; i++) {
+                        string folderPath = string.Join("/", path[..i]);
+                        if(folders.TryGetValue(folderPath, out RawFile folder1)) {
+                            parent = folder1;
+                            continue;
+                        }
+                        RawFile rawFile = new(path[i], Array.Empty<RawFile>());
+                        folders.Add(folderPath, rawFile);
+                        files.Add(rawFile);
+                        parent?.Files.Add(rawFile);
+                        parent = rawFile;
+                    }
+                }
+                folders[folder].Files.Add(new RawFile(path[^1], buffer));
+            } else files.Add(new RawFile(nameChanger(entry.FullName), buffer));
+        }
+        return files.ToArray();
+    }
+
     public static RawFile Unzip(string name, byte[] zipData) => new(name, Unzip(zipData));
 
     public static RawFile Unzip(string name, Stream stream) => new(name, Unzip(stream));
+
+    public static RawFile Unzip(string name, byte[] zipData, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) => new(name, Unzip(zipData, nameChanger, dataChanger));
+
+    public static RawFile Unzip(string name, Stream stream, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) => new(name, Unzip(stream, nameChanger, dataChanger));
 
     public static void Unzip(byte[] zipData, string path) {
         using MemoryStream zipStream = new(zipData);
@@ -68,9 +109,62 @@ public static class Zipper {
         }
     }
 
+    public static void Unzip(byte[] zipData, string path, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        using MemoryStream zipStream = new(zipData);
+        Unzip(zipStream, path, nameChanger, dataChanger);
+    }
+
+    public static void Unzip(Stream stream, string path, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        using ZipArchive archive = new(stream, ZipArchiveMode.Read, false, Encoding.UTF8);
+        foreach(ZipArchiveEntry entry in archive.Entries) {
+            string entryPath = Path.Combine(path, nameChanger(entry.FullName));
+            if(entryPath.EndsWith("/")) Directory.CreateDirectory(entryPath);
+            else {
+                string directory = Path.GetDirectoryName(entryPath);
+                if(!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+                using FileStream fileStream = File.Exists(entryPath) ? new FileStream(entryPath, FileMode.Open, FileAccess.Write, FileShare.None) : new FileStream(entryPath, FileMode.Create);
+                byte[] buffer = new byte[entry.Length];
+                using(Stream st = entry.Open()) st.Read(buffer, 0, buffer.Length);
+                buffer = dataChanger(buffer);
+                fileStream.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+
+    public static void Unzip(byte[] zipData, string path, Func<string, string> nameChanger, Stream writeStream, Stream readStream) {
+        using MemoryStream zipStream = new(zipData);
+        Unzip(zipStream, path, nameChanger, writeStream, readStream);
+    }
+
+    public static void Unzip(Stream stream, string path, Func<string, string> nameChanger, Stream writeStream, Stream readStream) {
+        using ZipArchive archive = new(stream, ZipArchiveMode.Read, false, Encoding.UTF8);
+        foreach(ZipArchiveEntry entry in archive.Entries) {
+            string entryPath = Path.Combine(path, nameChanger(entry.FullName));
+            if(entryPath.EndsWith("/")) Directory.CreateDirectory(entryPath);
+            else {
+                string directory = Path.GetDirectoryName(entryPath);
+                if(!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+                using FileStream fileStream = File.Exists(entryPath) ? new FileStream(entryPath, FileMode.Open, FileAccess.Write, FileShare.None) : new FileStream(entryPath, FileMode.Create);
+                using Stream st = entry.Open();
+                st.CopyTo(writeStream);
+                readStream.CopyTo(fileStream);
+            }
+        }
+    }
+
     public static void Unzip(string zipPath, string path) {
         using FileStream fileStream = new(zipPath, FileMode.Open);
         Unzip(fileStream, path);
+    }
+
+    public static void Unzip(string zipPath, string path, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        using FileStream fileStream = new(zipPath, FileMode.Open);
+        Unzip(fileStream, path, nameChanger, dataChanger);
+    }
+
+    public static void Unzip(string zipPath, string path, Func<string, string> nameChanger, Stream writeStream, Stream readStream) {
+        using FileStream fileStream = new(zipPath, FileMode.Open);
+        Unzip(fileStream, path, nameChanger, writeStream, readStream);
     }
 
     public static byte[] Zip(IEnumerable<RawFile> files) {
@@ -84,8 +178,38 @@ public static class Zipper {
         foreach(RawFile file in files) WriteZip(file, archive, null);
     }
 
+    public static byte[] Zip(IEnumerable<RawFile> files, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        using MemoryStream zipStream = new();
+        Zip(files, zipStream, nameChanger, dataChanger);
+        return zipStream.ToArray();
+    }
+
+    public static byte[] Zip(IEnumerable<RawFile> files, Func<string, string> nameChanger, Stream writeStream, Stream readStream) {
+        using MemoryStream zipStream = new();
+        Zip(files, zipStream, nameChanger, writeStream, readStream);
+        return zipStream.ToArray();
+    }
+
+    public static void Zip(IEnumerable<RawFile> files, Stream stream, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        using ZipArchive archive = new(stream, ZipArchiveMode.Create, false, Encoding.UTF8);
+        foreach(RawFile file in files) WriteZip(file, archive, null, nameChanger, dataChanger);
+    }
+
+    public static void Zip(IEnumerable<RawFile> files, Stream stream, Func<string, string> nameChanger, Stream writeStream, Stream readStream) {
+        using ZipArchive archive = new(stream, ZipArchiveMode.Create, false, Encoding.UTF8);
+        foreach(RawFile file in files) WriteZip(file, archive, null, nameChanger, writeStream, readStream);
+    }
+
     public static byte[] Zip(RawFile file) {
-        return file.IsFolder ? Zip(file.Files) : Zip(new[] { file });
+        return file.IsFolder ? Zip(file.Files) : Zip([file]);
+    }
+
+    public static byte[] Zip(RawFile file, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        return file.IsFolder ? Zip(file.Files, nameChanger, dataChanger) : Zip([file], nameChanger, dataChanger);
+    }
+
+    public static byte[] Zip(RawFile file, Func<string, string> nameChanger, Stream writeStream, Stream readStream) {
+        return file.IsFolder ? Zip(file.Files, nameChanger, writeStream, readStream) : Zip([file], nameChanger, writeStream, readStream);
     }
 
     private static void WriteZip(RawFile rawFile, ZipArchive archive, string folder) {
@@ -98,6 +222,30 @@ public static class Zipper {
         using Stream entryStream = entry.Open();
         entryStream.Write(rawFile.Data);
     }
+
+    private static void WriteZip(RawFile rawFile, ZipArchive archive, string folder, Func<string, string> nameChanger, Func<byte[], byte[]> dataChanger) {
+        string fileName = folder == null ? rawFile.Name : Path.Combine(folder, rawFile.Name);
+        if(rawFile.IsFolder) {
+            foreach(RawFile file in rawFile.Files) WriteZip(file, archive, fileName);
+            return;
+        }
+        ZipArchiveEntry entry = archive.CreateEntry(nameChanger(fileName));
+        using Stream entryStream = entry.Open();
+        entryStream.Write(dataChanger(rawFile.Data));
+    }
+
+    private static void WriteZip(RawFile rawFile, ZipArchive archive, string folder, Func<string, string> nameChanger, Stream writeStream, Stream readStream) {
+        string fileName = folder == null ? rawFile.Name : Path.Combine(folder, rawFile.Name);
+        if(rawFile.IsFolder) {
+            foreach(RawFile file in rawFile.Files) WriteZip(file, archive, fileName);
+            return;
+        }
+        ZipArchiveEntry entry = archive.CreateEntry(nameChanger(fileName));
+        using Stream entryStream = entry.Open();
+        writeStream.Write(rawFile.Data, 0, rawFile.Data.Length);
+        readStream.CopyTo(entryStream);
+    }
+
 
     public static byte[] Gunzip(byte[] gzipData) {
         using MemoryStream gzipStream = new(gzipData);
